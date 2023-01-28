38df459be3b15e965581b7360bd16192adbfb243
SamuelOuana <samuelouana5@gmail.com>
2023-01-28 16:07:09 +0200

    Ola mundo

---
+++ Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MsgBox("Bem vindo ao Microsoft Visual Basic 2010.")
+
+    End Sub
+End Class
